7060642a4f15b853b2d5b2892285a477c42b2572
patataowosi <patataowosi@gmail.com>
2024-10-15 14:31:04 +0000

    Update Main Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import './App.css'
-import Nll_Main from './pages/Nll_Main/Nll_Main'
 import Nll_Stream from './pages/Nll_Stream/Nll_Stream'
 
 function App() {
 	return (
 		<>
-			{/* <NllHeader />
-			<section className="Nll_section Nll_grid_layout">
-				<main className="Nll_vid_section" role="complementary">
-					<Nll_VideoPlayer
-					src="./video/Render.mp4"
-					poster="./img/thumb_01.png"
-					/>
-				</main>
-					<Nll_VideoAside	/>
-			</section>
-			<Nll_Footer /> */}
-			<Nll_Main />
+			<Nll_Stream />
 		</>
 	)
 }
